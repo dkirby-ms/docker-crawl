09f9529849743b65dd0d98f57404d4f50493339b
dkirby-ms <saitcho@outlook.com>
2021-09-24 18:54:31 -0500

    hmm again

--- crawl/util/webtiles-init-player.sh
+++ crawl/util/webtiles-init-player.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-
-RCDIR=/data/rcs/$CRAWLVERSION
-INPROGRESSDIR=/data/inprogress/$CRAWLVERSION
-TTYRECDIR=/data/ttyrec/$1
-MORGUEDIR=/data/morgue/$1
-DEFAULT_RC=/app/settings/init.txt
+INPROGRESSDIR=/tdata/inprogress/$CRAWLVERSION
+TTYRECDIR=/tdata/ttyrec/$1
+MORGUEDIR=/tdata/morgue/$1
+DEFAULT_RC=/webserver/settings/init.txt
 PLAYERNAME=$1
 
-mkdir -p $RCDIR
 mkdir -p $INPROGRESSDIR
 mkdir -p $TTYRECDIR
 mkdir -p $MORGUEDIR
 
+for CRAWLVERSION in git 26 27
+do
+RCDIR=/data/rcs/$CRAWLVERSION
+mkdir -p $RCDIR
 if [ ! -f ${RCDIR}/${PLAYERNAME}.rc ]; then
     cp ${DEFAULT_RC} ${RCDIR}/${PLAYERNAME}.rc
 fi
+done
