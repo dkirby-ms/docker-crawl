89cb2424a5c081dba3430779c6d0a9d68eefb972
dkirby-ms <saitcho@outlook.com>
2021-09-24 20:45:27 -0500

    websockets back to local file system

--- crawl/util/webtiles-init-player.sh
+++ crawl/util/webtiles-init-player.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-INPROGRESSDIR=/tdata/inprogress/$CRAWLVERSION
-TTYRECDIR=/tdata/ttyrec/$1
-MORGUEDIR=/tdata/morgue/$1
+TTYRECDIR=/data/ttyrec/$1
+MORGUEDIR=/data/morgue/$1
 DEFAULT_RC=/webserver/settings/init.txt
 PLAYERNAME=$1
 
-mkdir -p $INPROGRESSDIR
 mkdir -p $TTYRECDIR
 mkdir -p $MORGUEDIR
 
 for CRAWLVERSION in $SUPPORTED_CRAWL_VERSIONS
 do
 RCDIR=/data/rcs/$CRAWLVERSION
+INPROGRESSDIR=/data/inprogress/$CRAWLVERSION
 mkdir -p $RCDIR
+mkdir -p $INPROGRESSDIR
 if [ ! -f ${RCDIR}/${PLAYERNAME}.rc ]; then
     cp ${DEFAULT_RC} ${RCDIR}/${PLAYERNAME}.rc
 fi
